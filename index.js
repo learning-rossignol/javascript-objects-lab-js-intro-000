195a5ef817208b8d1441f9f518e43b381ed4de94
Jean-François Rompré <jrompre@gmail.com>
2019-10-11 18:15:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function updateObjectWithKeyAndValue(obj, k, v) {
   return Object.assign({}, obj, {[k]: v});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, k, v) {
+  return Object.assign(obj, {[k]: v});
+}
