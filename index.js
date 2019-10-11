ff8a94bcb01e6fdc78ff5e6540aba6592c963c6c
Jean-François Rompré <jrompre@gmail.com>
2019-10-11 18:18:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, k, v) {
 var recipes = {};
 
 function deleteFromObjectByKey(obj, k) {
-  var new-obj = Object.assign({}, obj);
-  delete new-obj[k];
-  return new-obj;
+  var newobj = Object.assign({}, obj);
+  delete newobj[k];
+  return newobj;
 }
